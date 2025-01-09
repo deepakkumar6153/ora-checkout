6c9e2bdcfdc75256013e1367c699e9d7ac08adee
dipak.iitk@gmail.com <dipak.iitk@gmail.com>
2025-01-09 12:57:28 +0530

    gh-pages script commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   reactStrictMode: true,
-  distDir: "dist",
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? "/ora-checkout/" : "",
+  basePath: isProd ? "/ora-checkout" : "",
   output: "export",
 };
+
 export default nextConfig;
